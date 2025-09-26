236929ef6e8ca9a9d6cc8e82fbb9d8c004458571
Syed Shoeb Abrar <99830705+SyedShoeb22@users.noreply.github.com>
2025-09-26 15:05:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,3 @@
-# -------------------------------
-# OCI Provider Variables
-# -------------------------------
 variable "tenancy_ocid" {
   description = "OCI Tenancy OCID"
   type        = string
@@ -17,74 +14,16 @@ variable "fingerprint" {
 }
 
 variable "private_key_path" {
-  description = "Path to OCI API Private Key"
+  description = "Path to the private API key"
   type        = string
 }
 
 variable "region" {
-  description = "OCI Region"
+  description = "OCI region"
   type        = string
-  default     = "ap-mumbai-1"
 }
 
 variable "compartment_ocid" {
-  description = "OCI Compartment OCID where resources will be created"
+  description = "Target compartment OCID"
   type        = string
-}
-
-# -------------------------------
-# Networking Variables
-# -------------------------------
-variable "vcn_cidr" {
-  description = "CIDR block for the VCN"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet_cidr" {
-  description = "CIDR block for the subnet"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "availability_domain_index" {
-  description = "Index of the availability domain (0,1,2)"
-  type        = number
-  default     = 0
-}
-
-# -------------------------------
-# Compute Instance Variables
-# -------------------------------
-variable "instance_shape" {
-  description = "OCI Compute shape"
-  type        = string
-  default     = "VM.Standard.E2.1.Micro"
-}
-
-variable "instance_display_name" {
-  description = "Display name for the instance"
-  type        = string
-  default     = "sre-lab-instance"
-}
-
-variable "ssh_public_key_path" {
-  description = "Path to SSH public key for instance access"
-  type        = string
-  default     = "~/.ssh/id_rsa.pub"
-}
-
-# -------------------------------
-# Optional Tags
-# -------------------------------
-variable "defined_tags" {
-  description = "Defined tags for resources"
-  type        = map(string)
-  default     = {}
-}
-
-variable "freeform_tags" {
-  description = "Freeform tags for resources"
-  type        = map(string)
-  default     = {}
 }
