46f4ae58949a48999684e2ee0570ecdff2b056d8
Syed Shoeb Abrar <99830705+SyedShoeb22@users.noreply.github.com>
2025-09-26 15:10:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,15 +11,15 @@ data "oci_identity_availability_domains" "ads" {
   compartment_id = var.tenancy_ocid
 }
 
-# Fetch latest Oracle Linux image
+# Fetch latest Oracle Linux image (no shape restriction)
 data "oci_core_images" "oracle_linux" {
   compartment_id           = var.compartment_ocid
   operating_system         = "Oracle Linux"
   operating_system_version = "8"
-  shape                    = "VM.Standard.E2.1.Micro"
   sort_by                  = "TIMECREATED"
   sort_order               = "DESC"
 }
+
 
 # Create compute instance
 resource "oci_core_instance" "sre_instance" {
@@ -30,11 +30,12 @@ resource "oci_core_instance" "sre_instance" {
   create_vnic_details {
     subnet_id = oci_core_subnet.sre_subnet.id
   }
-
+  
   source_details {
     source_type = "image"
     source_id   = data.oci_core_images.oracle_linux.images[0].id
   }
+
 
   display_name = "sre-instance"
 }
